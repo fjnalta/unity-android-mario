52d365a204356f46df025bebaa532e0eb88c6cb0
Philipp Minges <able@ezlife.eu>
2016-12-29 13:42:02 +0100

    implement UIcontroller

---
+++ Assets/Scripts/Level/UIController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIController : MonoBehaviour {
+
+	private float timeLeft = 300f;
+	public Text timeLeftText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		countDown ();
+	}
+
+	void countDown(){
+		timeLeft -= Time.deltaTime;
+		timeLeftText.text = "TIME: " + Mathf.Round(timeLeft);
+//		if(timeLeft < 0)
+//		{
+//			Application.LoadLevel("gameOver");
+//		}
+	}
+}
